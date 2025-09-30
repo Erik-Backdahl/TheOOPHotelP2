24c8d9cf305131b42f1122eb35fd06a74ff86c19
Emma Karlholm <emma.karlholm@gmail.com>
2025-09-30 10:20:01 +0200

    Added consultant

---
+++ classes/Consultant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace TheOOPHotelP2
+{
+	internal class Consultant
+	{
+		public decimal HourlyRate { get; set; }
+		public string ConsultingFirm { get; set; }
+
+
+		public Consultant(decimal hourlyRate, string consultingFirm) : base(name)
+		{
+			HourlyRate = hourlyRate;
+			ConsultingFirm = consultingFirm;
+		}
+		
+		public GiveAdvice()
+		{
+			Console.WriteLine($"{name} gives advice on how the hotel may improve their routines and services.");
+		}
+	}
+}
