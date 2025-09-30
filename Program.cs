6892a45a9f7c7b848005c001267c78cf15b8faf2
Daniel Hultmark <danielhultmark@gmail.com>
2025-09-30 10:26:06 +0200

    Created testpersons

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Manager manager = new Manager();
+    Name = "Daniel Hultmark"
+    Age = 31
+    EmployeId = "DH007"
+    StarDate = new DateOnly(2020, 01, 15)
+    Salary = 50000m
+    Deparment = "Adminstrator"
+
+Employee employee = new Employee();
+    Name = "Anna Andersson"
+    Age = 28
+    EmployeId = "AA123"
+    StartDate = new DateOnly(2021, 06, 01)
+    Salary = 35000m
+    JobTitle = "Receptionist"
