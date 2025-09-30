d9177b37f3b49e948d6d49e11afd32d16b986fbc
Emma Karlholm <emma.karlholm@gmail.com>
2025-09-30 10:35:53 +0200

    Added housekeeper

---
+++ classes/Housekeeper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TheOOPHotelP2
+{
+	public class Housekeeper : Person
+	{
+
+
+		public Housekeeper()
+		{
+
+		}
+
+
+		public override Work()
+		{
+			Console.WriteLine($"{Name} cleans the hotel rooms.");
+		}
+	}
+}
